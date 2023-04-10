208de88a5d471ad787fa666726b78a95d7bb5c38
Sergey Ryazanov <majo78@mail.ru>
2023-04-10 05:40:40 +0300

    Add binance api

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+// Convert milliseconds to time.Time
+func MillisecondsToTime(milliseconds int64) time.Time {
+	return time.Unix(0, milliseconds*int64(time.Millisecond))
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println("----------------------")
+	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
+	if exists {
+		fmt.Println("apiKey exist")
+	}
+
+	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
+	if exexists {
+		fmt.Println("secretKey exist")
+	}
+
+	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
+	res, err := futuresClient.NewDepthService().Symbol("BTCUSDT").Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
 }
